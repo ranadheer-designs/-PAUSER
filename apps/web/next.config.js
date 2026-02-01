a963ac4391da7345c565bf606d2c759a825758cb
ranadheer-designs <ranadheerpothula@gmail.com>
2026-02-01 20:07:01 +0530

    Fix Vercel build: Add webpack aliases for monorepo packages

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@pauser/common', '@pauser/ui'],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@pauser/common': require('path').resolve(__dirname, '../../packages/common/src/index.ts'),
+      '@pauser/ui': require('path').resolve(__dirname, '../../packages/ui/src/index.ts'),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
